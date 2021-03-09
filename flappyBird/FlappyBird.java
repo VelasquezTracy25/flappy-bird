48f7887583e1c8326e6ab8fb5be6c01e0fabc73e
Tracy Velasquez <velasqueztracy25@gmail.com>
2021-03-08 22:01:01 -0600

    Added color components

--- flappyBird/FlappyBird.java
+++ flappyBird/FlappyBird.java
@@ -1,8 +1,18 @@
 package flappyBird;
 
-import javax.swing.JFrame;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.Rectangle;
 
-public class FlappyBird {
+
+import javax.swing.JFrame;
+import javax.swing.Timer;
+
+
+public class FlappyBird implements ActionListener
+{
 
     public static FlappyBird flappyBird;
 
@@ -10,19 +20,43 @@ public class FlappyBird {
 
     public Renderer renderer;
 
+    public Rectangle bird;
+
     public FlappyBird(){
         JFrame jframe = new JFrame();
+
+        Timer timer = new Timer(20, this);
+
+        renderer = new Renderer();
 
         jframe.add(renderer);
         jframe.setSize(WIDTH, HEIGHT);
         jframe.setVisible(true);
         jframe.setResizable(false);
         jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        bird = new Rectangle(WIDTH / 2 - 10, HEIGHT / 2 - 10, 20, 20 );
+
+        timer.start();
     }
 
     public void repaint(Graphics g){
+        g.setColor(Color.cyan);
+        g.fillRect(0, 0, WIDTH, HEIGHT);
 
+        g.setColor(Color.orange);
+        g.fillRect(0, HEIGHT - 150, WIDTH, 150);
+
+        g.setColor(Color.red);
+        g.fillRect(0, HEIGHT- 150, WIDTH, 20);
     }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+            renderer.repaint();        
+    }
+
+
     public static void main(String[] args){
         flappyBird = new FlappyBird();
 
